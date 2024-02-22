f42f0e40869f54a8da0fd89421c8219ed0325e16
Raj Thimmiah <rajlego@gmail.com>
2024-02-22 22:42:18 +0530

    trying new box

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const { create } = require("domain");
+const { text } = require("stream/consumers");
+
 document.getElementById('catButton').addEventListener('click', function() {
     document.getElementById('catDisplay').textContent = 'Cat';
 });
+
+document.getElementById('addInput').addEventListener("click", function() {
+    var input = document.createElement('input')
+
+    input.type = 'text'
+    input.placeholder = 'Enter something!'
+
+    document.getElementById('inputList').appendChild(input)
+})
